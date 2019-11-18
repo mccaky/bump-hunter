2850ea06a01f315171022d987e2ca4da4fd1b9cd
mccaky <mccarty@jlab.org>
2019-11-18 17:30:58 -0500

    Modified cmake instructions for the math package to remove the old background-specific fit functions.

--- src/math/ExpPol3BkgFunction.cxx
+++
@@ -1,7 +0,0 @@
-#include <ExpPol3BkgFunction.h>
-#include <FunctionMath.h>
-
-double ExpPol3BkgFunction::calculateBackground(double *x, double *par) {
-	double xp = FitFunction::getCorrectedX(x[0]);
-	return FunctionMath::Order3BackgroundFunction(xp, par[0], par[1], par[2], par[3]);
-}
--- src/math/ExpPol5BkgFunction.cxx
+++
@@ -1,7 +0,0 @@
-#include <ExpPol5BkgFunction.h>
-#include <FunctionMath.h>
-
-double ExpPol5BkgFunction::calculateBackground(double *x, double *par) {
-	double xp = FitFunction::getCorrectedX(x[0]);
-	return FunctionMath::Order5BackgroundFunction(xp, par[0], par[1], par[2], par[3], par[4], par[5]);
-}
